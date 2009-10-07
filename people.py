8fbc2b140e1f91e08dd69fc02f7ea4273dfa5213
ssp <ssp@030910c9-d52c-0410-92f0-b9f1c9c7ad2e>
2009-10-07 16:00:25 +0000

    tut, was es soll, Namen in separate Datei gelegt
    git-svn-id: file:///Users/ssp/Developer/svn@452 030910c9-d52c-0410-92f0-b9f1c9c7ad2e

---
+++ people.py
@@ -0,0 +1,41 @@
+#coding=utf-8
+"""
+people.py
+arXivToWiki v1
+©2009 by Sven-S. Porst / earthlingsoft (ssp-web@earthlingsoft.net)
+
+List of people and their web pages for automatic linking in arXivToWiki.
+"""
+
+
+"""
+	Set up table of real names and crcg user names to create links to people's pages.
+	
+	Strings containing non-ASCII characters need to be prefixed by a u. E.g.: u"Müller"
+"""
+people = dict({
+"Laurent Bartholdi": "http://www.uni-math.gwdg.de/laurent/",
+"Ralf Meyer": "http://www.uni-math.gwdg.de/rameyer/",
+"Preda Mihailescu": "http://www.uni-math.gwdg.de/preda/",
+"Samuel J. Patterson": "http://www.uni-math.gwdg.de/sjp/",
+"Pablo Ramacher": "http://www.uni-math.gwdg.de/ramacher/",
+"Karl-Henning Rehren": "http://www.theorie.physik.uni-goe.de/~rehren/",
+"Thomas Schick": "http://www.uni-math.gwdg.de/schick/",
+"Andreas Thom": "http://www.math.uni-leipzig.de/MI/thom/",
+"Yuri Tschinkel": "http://www.uni-math.gwdg.de/tschinkel/",
+"Ingo Witt": "http://www.uni-math.gwdg.de/iwitt/",
+"Dorothea Bahns": "http://www.uni-math.gwdg.de/bahns/",
+"Hannah Markwig": "User:Hannah",
+"Chenchang Zhu": "User:Zhu",
+"Anders Jensen": "User:Jensen",
+"Alessandro Valentino": "http://www.uni-math.gwdg.de/sandro/",
+"Hans-Christian Graf v. Bothmer": "User:Bothmer",
+"Hans-Christian Graf von Bothmer": "User:Bothmer",
+"Hans-Christian Bothmer": "User:Bothmer",
+"Hans-Christian v. Bothmer": "User:Bothmer",
+"Hans-Christian von Bothmer": "User:Bothmer",
+"Thomas Markwig": "http://www.mathematik.uni-kl.de/~keilen/de/index.html",
+"Eugenii Shustin": "http://www.math.tau.ac.il/~shustin/",
+u"Christian Böhning": "http://www.uni-math.gwdg.de/boehning/",
+"Christian Boehning": "http://www.uni-math.gwdg.de/boehning/"
+})
