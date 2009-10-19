12919b668f11a4258ca3af9d6eb3e66b5945952f
ssp <ssp@030910c9-d52c-0410-92f0-b9f1c9c7ad2e>
2009-10-19 00:02:17 +0000

    Add querying author IDs. Clean up entered IDs a little before submitting them. Improved error messages.
    git-svn-id: file:///Users/ssp/Developer/svn@453 030910c9-d52c-0410-92f0-b9f1c9c7ad2e

--- people.py
+++ people.py
@@ -9,33 +9,49 @@ List of people and their web pages for automatic linking in arXivToWiki.
 
 
 """
-	Set up table of real names and crcg user names to create links to people's pages.
-	
-	Strings containing non-ASCII characters need to be prefixed by a u. E.g.: u"Müller"
+	Set up table of real names pointing to a dictionary with
+		URL     -> full web page URL for the person
+		arXivID -> arXiv ID for the person
+		
+	Be sure to precede the name string with a u, e.g. u"Carl Müller".
 """
+
+URL = "URL"
+arXivID = "arXiv ID"
+
 people = dict({
-"Laurent Bartholdi": "http://www.uni-math.gwdg.de/laurent/",
-"Ralf Meyer": "http://www.uni-math.gwdg.de/rameyer/",
-"Preda Mihailescu": "http://www.uni-math.gwdg.de/preda/",
-"Samuel J. Patterson": "http://www.uni-math.gwdg.de/sjp/",
-"Pablo Ramacher": "http://www.uni-math.gwdg.de/ramacher/",
-"Karl-Henning Rehren": "http://www.theorie.physik.uni-goe.de/~rehren/",
-"Thomas Schick": "http://www.uni-math.gwdg.de/schick/",
-"Andreas Thom": "http://www.math.uni-leipzig.de/MI/thom/",
-"Yuri Tschinkel": "http://www.uni-math.gwdg.de/tschinkel/",
-"Ingo Witt": "http://www.uni-math.gwdg.de/iwitt/",
-"Dorothea Bahns": "http://www.uni-math.gwdg.de/bahns/",
-"Hannah Markwig": "User:Hannah",
-"Chenchang Zhu": "User:Zhu",
-"Anders Jensen": "User:Jensen",
-"Alessandro Valentino": "http://www.uni-math.gwdg.de/sandro/",
-"Hans-Christian Graf v. Bothmer": "User:Bothmer",
-"Hans-Christian Graf von Bothmer": "User:Bothmer",
-"Hans-Christian Bothmer": "User:Bothmer",
-"Hans-Christian v. Bothmer": "User:Bothmer",
-"Hans-Christian von Bothmer": "User:Bothmer",
-"Thomas Markwig": "http://www.mathematik.uni-kl.de/~keilen/de/index.html",
-"Eugenii Shustin": "http://www.math.tau.ac.il/~shustin/",
-u"Christian Böhning": "http://www.uni-math.gwdg.de/boehning/",
-"Christian Boehning": "http://www.uni-math.gwdg.de/boehning/"
+u"Laurent Bartholdi":                dict(URL="http://www.uni-math.gwdg.de/laurent/", arXivID="bartholdi_l_1"),
+u"L. Bartholdi":                     dict(URL="http://www.uni-math.gwdg.de/laurent/", arXivID="bartholdi_l_1"),
+u"Ralf Meyer":                       dict(URL="http://www.uni-math.gwdg.de/rameyer/", arXivID="meyer_r_1"),
+u"R. Meyer":                         dict(URL="http://www.uni-math.gwdg.de/rameyer/", arXivID="meyer_r_1"),
+u"Preda Mihailescu":                 dict(URL="http://www.uni-math.gwdg.de/preda/"),
+u"Samuel J. Patterson":              dict(URL="http://www.uni-math.gwdg.de/sjp/"),
+u"Pablo Ramacher":                   dict(URL="http://www.uni-math.gwdg.de/ramacher/"),
+u"Karl-Henning Rehren":              dict(URL="http://www.theorie.physik.uni-goe.de/~rehren/"),
+u"Thomas Schick":                    dict(URL="http://www.uni-math.gwdg.de/schick/", arXivID="schick_t_1"),
+u"T. Schick":                        dict(URL="http://www.uni-math.gwdg.de/schick/", arXivID="schick_t_1"),
+u"Andreas Thom":                     dict(URL="http://www.math.uni-leipzig.de/MI/thom/"),
+u"Yuri Tschinkel":                   dict(URL="http://www.uni-math.gwdg.de/tschinkel/"),
+u"Ingo Witt":                        dict(URL="http://www.uni-math.gwdg.de/iwitt/"),
+u"Dorothea Bahns":                   dict(URL="http://www.uni-math.gwdg.de/bahns/"),
+u"Hannah Markwig":                   dict(URL="http://www.crcg.de/wiki/User:Hannah", arXivID="markwig_h_1"),
+u"H. Markwig":                       dict(URL="http://www.crcg.de/wiki/User:Hannah", arXivID="markwig_h_1"),
+u"Chenchang Zhu":                    dict(URL="http://www.crcg.de/wiki/User:Zhu", arXivID="zhu_c_1"),
+u"C. Zhu":                           dict(URL="http://www.crcg.de/wiki/User:Zhu", arXivID="zhu_c_1"),
+u"Anders Jensen":                    dict(URL="http://www.crcg.de/wiki/User:Jensen"),
+u"Anders Nedergaard Jensen":         dict(URL="http://www.crcg.de/wiki/User:Jensen"),
+u"Alessandro Valentino":             dict(URL="http://www.uni-math.gwdg.de/sandro/", arXivID="valentino_a_1"),
+u"Hans-Christian Graf v. Bothmer":   dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"Hans-Christian Graf von Bothmer":  dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"Hans-Christian Bothmer":           dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"Hans-Christian v. Bothmer":        dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"Hans-Christian von Bothmer":       dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"H.-Chr. Graf von Bothmer":         dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"H. -Chr. Graf von Bothmer":        dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"H.-C. Graf v. Bothmer":            dict(URL="http://www.crcg.de/wiki/User:Bothmer", arXivID="grafvbothmer_h_1"),
+u"Thomas Markwig": 	                 dict(URL="http://www.mathematik.uni-kl.de/~keilen/de/index.html"),
+u"Thomas Keilen": 	                 dict(URL="http://www.mathematik.uni-kl.de/~keilen/de/index.html"),
+u"Eugenii Shustin":                  dict(URL="http://www.math.tau.ac.il/~shustin/"),
+u"Christian Böhning":                dict(URL="http://www.uni-math.gwdg.de/boehning/"),
+u"Jakob Kröker":                     dict(URL="http://www.crcg.de/wiki/User:Kroeker")
 })
